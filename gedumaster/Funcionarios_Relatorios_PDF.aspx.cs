d5a76beeaa378ad6965ba4e8fe06b873ec302847
Carlos Sales <causalvador@hotmail.com>
2017-09-12 23:25:59 -0300

    pdf header footer

--- gedumaster/Funcionarios_Relatorios_PDF.aspx.cs
+++ gedumaster/Funcionarios_Relatorios_PDF.aspx.cs
@@ -142,7 +142,7 @@ public partial class Funcionarios_Relatorios_PDF : System.Web.UI.Page
     private void MontaPDF()
     {
 
-        Document doc = new Document(PageSize.A4.Rotate(), 20, 20, 20, 20);
+        Document doc = new Document(PageSize.A4.Rotate(), 40, 40, 60, 60);
         System.IO.MemoryStream str = new System.IO.MemoryStream();
         PdfWriter writer = PdfWriter.GetInstance(doc, str);
 
@@ -157,37 +157,11 @@ public partial class Funcionarios_Relatorios_PDF : System.Web.UI.Page
         ////// corpo do PDF
         #region
         doc.Open();
-
-        PdfPTable tab = new PdfPTable(3);
-        PdfPCell cell = new PdfPCell(new Phrase("Header", new Font(Font.FontFamily.HELVETICA, 24F)));
-
-        cell.Colspan = 3;
-        cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
-        cell.BorderColor = new BaseColor(System.Drawing.Color.Red);
-        cell.Border = Rectangle.BOTTOM_BORDER; // | Rectangle.TOP_BORDER;
-        cell.BorderWidthBottom = 3f;
-        tab.AddCell(cell);
-
-        //row 1
-        tab.AddCell("R1C1");
-        tab.AddCell("R1C2");
-        tab.AddCell("R1C3");
-        //row 2
-        tab.AddCell("R2C1");
-        tab.AddCell("R2C2");
-        tab.AddCell("R2C3");
-        cell = new PdfPCell();
-        cell.Colspan = 3;
-
-        iTextSharp.text.List pdfList = new List(List.UNORDERED);
-        pdfList.Add(new iTextSharp.text.ListItem(new Phrase("Unorder List 1")));
-        pdfList.Add("Unorder List 2");
-        pdfList.Add("Unorder List 3");
-        pdfList.Add("Unorder List 4");
-        cell.AddElement(pdfList);
-        tab.AddCell(cell);
-        doc.Add(tab);
-
+        for (int i = 0; i < 3; i++)
+        {
+            doc.Add(new Paragraph("Jesus é Bom" ));
+            doc.NewPage();
+        }
         doc.Close();
 
         Response.Clear();
@@ -197,7 +171,9 @@ public partial class Funcionarios_Relatorios_PDF : System.Web.UI.Page
         Response.OutputStream.Write(str.GetBuffer(), 0, str.GetBuffer().Length);
         Response.OutputStream.Flush();
         Response.End();
+
         #endregion 
+
     }
 
     public class PDFHeaderFooter : PdfPageEventHelper
@@ -235,19 +211,20 @@ public partial class Funcionarios_Relatorios_PDF : System.Web.UI.Page
 
         public override void OnEndPage(PdfWriter writer, Document document)
         {
-         
-            base.OnEndPage(writer, document);
 
-            PdfPTable HeaderPag = new PdfPTable(1);
-            HeaderPag.TotalWidth = 300F;
+            PdfContentByte cb = writer.DirectContent;
+            Phrase header = new Phrase(Header_a, ffont);
+            Phrase footer = new Phrase(Footer_a, ffont);
 
-            PdfPCell cell = new PdfPCell(new Phrase(Header_a, ffont));
-            cell.HorizontalAlignment = 1;  //0=Left, 1=Centre, 2=Right
-            HeaderPag.AddCell(cell);
+            ColumnText.ShowTextAligned(cb, Element.ALIGN_CENTER,
+                    header,
+                    (document.Right - document.Left) / 2 + document.LeftMargin,
+                    document.Top + 30, 0);
 
-            HeaderPag.WriteSelectedRows(0, -1, 150, document.Top, writer.DirectContent);
-            
-
+            ColumnText.ShowTextAligned(cb, Element.ALIGN_CENTER,
+                    footer,
+                    (document.Right - document.Left) / 2 + document.LeftMargin,
+                    document.Bottom - 30, 0);
         }
 
     }
