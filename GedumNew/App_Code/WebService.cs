cad25e6021aa4c88ad82699b6e20d92ddcaf67cc
Carlos Sales <causalvador@hotmail.com>
2017-10-09 23:07:46 -0300

    menu Master - ok

---
+++ GedumNew/App_Code/WebService.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string TentaLogin(string user, string pwd)
+    {
+        string Identificador_msg = "0";
+
+        // localiza usuario
+        string stringSelect = "select senha,nome,ID_user,ID_Munic, nivel from tbl_usuarios where usuario = '" + user + "'";
+        OperacaoBanco Identificador_Operacao = new OperacaoBanco();
+        SqlDataReader Identificador_rcrdset = Identificador_Operacao.Select(stringSelect);
+        while (Identificador_rcrdset.Read())
+        {
+            if (pwd == Convert.ToString(Identificador_rcrdset[0]))
+            {
+
+                int nivel = Convert.ToInt16(Identificador_rcrdset[4]);
+
+                if (nivel > 1)
+                {
+                    // nivel 2 em diante não tem acesso a este modulo/master
+                    Identificador_msg = "1";
+                }
+                else
+                {
+
+                    string vValida1, vValida2;
+                    vValida1 = DateTime.Now.ToString("dd");
+                    vValida2 = DateTime.Now.ToString("MM");
+                    int vValida3 = Convert.ToInt16(vValida1) * Convert.ToInt16(vValida2);
+                    string vValida4 = vValida3.ToString();
+
+                    Identificador_msg = "http://gedumaster.azurewebsites.net/LogIn.aspx" +
+                        "?p1=" + vValida4 +
+                        "&p2=" + Convert.ToString(Identificador_rcrdset[1]) +
+                        "&p3=" + Convert.ToString(Identificador_rcrdset[2]) +
+                        "&p4=" + nivel +
+                        "&p5=" + Convert.ToString(Identificador_rcrdset[3]);
+                }
+            }
+            else
+            {
+                Identificador_msg = "2";
+            }
+        }
+        ConexaoBancoSQL.fecharConexao();
+
+        return Identificador_msg;
+
+    }
+
+    public class ConexaoBancoSQL
+    {
+        private static SqlConnection objConexao = null;
+        private string stringconnection1;
+
+        public void tentarAbrirConexaoRemota()
+        {
+            objConexao = new SqlConnection();
+            objConexao.ConnectionString = stringconnection1;
+            objConexao.Open();
+        }
+
+        public ConexaoBancoSQL()
+        {
+            // <!--*******Customização*******-->
+            stringconnection1 = "Server=tcp:servereducacao.database.windows.net,1433;Initial Catalog=dbeducacao;Persist Security Info=False;User ID=admserver;Password=Pwd@2017;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            try
+            {
+                tentarAbrirConexaoRemota();
+            }
+            catch
+            {
+                Console.WriteLine("Atenção! Não foi possível Conectar ao Servidor de Banco de Dados.");
+            }
+        }
+
+        public static SqlConnection getConexao()
+        {
+            new ConexaoBancoSQL();
+            return objConexao;
+        }
+        public static void fecharConexao()
+        {
+            objConexao.Close();
+        }
+    }
+
+    public class OperacaoBanco
+    {
+        private SqlCommand TemplateMethod(String query)
+        {
+            SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+            SqlCommand Commando = new SqlCommand(query, Conexao);
+            try
+            {
+                Commando.ExecuteNonQuery();
+                return Commando;
+            }
+            catch
+            {
+                return Commando;
+            }
+        }
+
+        public SqlDataReader Select(String query)
+        {
+            SqlDataReader dadosObtidos = TemplateMethod(query).ExecuteReader();
+            return dadosObtidos;
+        }
+
+        public Boolean Insert(String query)
+        {
+            SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+            SqlCommand Commando = new SqlCommand(query, Conexao);
+            try
+            {
+                Commando.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Boolean Update(String query)
+        {
+            SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+            SqlCommand Commando = new SqlCommand(query, Conexao);
+            try
+            {
+                Commando.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Boolean Delete(String query)
+        {
+            SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+            SqlCommand Commando = new SqlCommand(query, Conexao);
+            try
+            {
+                Commando.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+}
