aad00d0673520f3b8d1da31119440de444cf2986
Carlos Sales <causalvador@hotmail.com>
2017-11-28 18:34:19 -0300

    patrimonio em andamento

---
+++ gedumWebApp/Patrimonio_Novo.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public partial class Patrimonio_Novo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //ID da Instituição
+        string InstID = Session["InstID"].ToString();
+
+        string ScriptAux = "<script type=\"text/javascript\">" +
+                        "document.getElementById('IDInstHidden').value = \"" + InstID + "\";" +
+                        "</script>";
+        Literal1.Text = ScriptAux;
+    }
+}
