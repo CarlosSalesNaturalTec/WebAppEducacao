65bd17da43c3aa0e307512ca6d48c601e19868e9
Douglas <douglasprogramador@hotmail.com>
2017-11-28 19:16:52 -0300

    merge

--- gedumWebApp/App_Code/WebService.cs
+++ gedumWebApp/App_Code/WebService.cs
@@ -880,7 +880,7 @@ public class WebService : System.Web.Services.WebService
     }
 
     [WebMethod]
-    public string FornecedorAlimentosAlterar(string param0, string param1, string param2, string param3, string param4, string param6,
+    public string FornecedorAlimentosAlterar(string param0, string param1, string param2, string param3, string param4, string param5, string param6,
         string param7, string param8, string param9, string param10, string param11, string param12, string param13, string param14)
     {
 
